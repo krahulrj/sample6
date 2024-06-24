cd3528da9e67929b41d15f027c44625ad1a41c4b
krahulrj <krahulrj24@gmail.com>
2024-06-24 15:19:57 +0530

    Adding Even Odd Program in c

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("Enter Number :");
+	scanf("%d",&n);
+	
+	if (n%2==0){
+	printf("It is Even Number");
+	}
+	else
+	{
+	printf("It is Odd Number");
+	}
+	printf("\n");
+}
